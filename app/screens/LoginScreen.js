2df9d8a762639ffe5441e6a6ea6f9af9400ee535
gchariton <gc@icloud.gr>
2024-02-28 20:58:32 +0200

    Some sounds added

---
+++ app/screens/LoginScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+function LoginScreen(props) {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>Login</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {},
+    text: {},
+});
+
+export default LoginScreen;
